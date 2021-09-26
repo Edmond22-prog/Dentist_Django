77e8633f741370f9c3c0c6adf110a34000a5740a
Edmond22-prog <ghislain.makolle@facsciences-uy1.cm>
2021-09-26 21:53:26 +0100

    setting of the appointment form and custom of the appointment page

--- dentist/website/views.py
+++ dentist/website/views.py
@@ -33,3 +33,35 @@ def pricing(request):
 
 def service(request):
     return render(request, "service.html", {})
+
+
+def appointment(request):
+    #Verifie s'il s'agit d'un POST ou d'un clique sur le lien
+    if (request.method == "POST"):
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_address = request.POST['your-address']
+        your_schedule = request.POST['your-schedule']
+        your_date = request.POST['your-date']
+        your_message = request.POST['your-message']
+
+        appointment = "Name: "+ your_name + " Phone: "+ your_phone +" Email: "+ your_email +" Address: "+ your_address +" Schedule: "+ your_schedule +" Day: "+ your_date +" Message: "+ your_message
+        # Envoi d'un email
+        send_mail (
+            'Appointment Request', # Sujet
+            appointment, # Message
+            your_email, # Email de l'envoyeur
+            ['edghimakoll@gmail.com'], # Email du receveur
+        )
+        return render(request, "appointment.html", {
+            'your_name': your_name,
+            'your_phone': your_phone,
+            'your_email': your_email,
+            'your_address': your_address,
+            'your_schedule': your_schedule,
+            'your_date': your_date,
+            'your_message': your_message
+            })
+    else:
+        return render(request, "home.html", {})
